03d0de28467a810a391ced2a31259e659558d398
Ketaki <urankarketaki@gmail.com>
2021-11-03 19:20:46 +0530

    Table added

--- src/pages/company/Company.js
+++ src/pages/company/Company.js
@@ -1,12 +1,62 @@
 import React from "react";
+import Table from "react-bootstrap/Table";
 function InsuranceCompany() {
   return (
     <div className="App">
-      <div className="formField">
-        <button type="submit" className="formFieldButton">
-          Pay
-        </button>{" "}
-      </div>
+      <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>Sr No.</th>
+              <th>Patient Name</th>
+              <th>Claim Date</th>
+              <th>Images</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>
+                <div className="formField">
+                  <button type="submit" className="formFieldButton">
+                    Pay
+                  </button>{" "}
+                  
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+              <td>
+                <div className="formField">
+                  <button type="submit" className="formFieldButton">
+                    Pay
+                  </button>{" "}
+                  
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td colSpan="2">Larry the Bird</td>
+              <td>@twitter</td>
+              <td>
+                <div className="formField">
+                  <button type="submit" className="formFieldButton">
+                    Pay
+                  </button>{" "}
+                  
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
     </div>
   );
 }
--- src/pages/staff/index.js
+++ src/pages/staff/index.js
@@ -1,17 +1,70 @@
 import React from "react";
+import Table from "react-bootstrap/Table";
 import "C:/Users/Ketaki/Desktop/bctprojectui-main/src/App.css";
 function HospitalStaff() {
   return (
     <div>
       <div className="App">
-        <div className="formField">
-          <button type="submit" className="formFieldButton">
-            Accept
-          </button>{" "}
-          <button type="submit" className="formFieldButton ">
-            Reject
-          </button>
-        </div>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Sr No.</th>
+              <th>Insurance Company Name </th>
+              <th>Claim Date</th>
+              <th>Images</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>
+                <div className="formField">
+                  <button type="submit" className="formFieldButton">
+                    Accept
+                  </button>{" "}
+                  <button type="submit" className="formFieldButton ">
+                    Reject
+                  </button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+              <td>
+                <div className="formField">
+                  <button type="submit" className="formFieldButton">
+                    Accept
+                  </button>{" "}
+                  <button type="submit" className="formFieldButton ">
+                    Reject
+                  </button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td colSpan="2">Larry the Bird</td>
+              <td>@twitter</td>
+              <td>
+                <div className="formField">
+                  <button type="submit" className="formFieldButton">
+                    Accept
+                  </button>{" "}
+                  <button type="submit" className="formFieldButton ">
+                    Reject
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
       </div>
       );
     </div>
